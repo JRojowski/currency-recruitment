a70600f4281d6e346f5877b5cb253eac030fd8ff
JRojowski <jacentylbn@gmail.com>
2024-11-21 16:57:49 +0100

    Initial commit

---
+++ src/main/java/io/github/JRojowski/currency_recruitment/CurrencyRecruitmentApplication.java
@@ -0,0 +1,13 @@
+package io.github.JRojowski.currency_recruitment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyRecruitmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyRecruitmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/JRojowski/currency_recruitment/CurrencyRecruitmentApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.JRojowski.currency_recruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyRecruitmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
