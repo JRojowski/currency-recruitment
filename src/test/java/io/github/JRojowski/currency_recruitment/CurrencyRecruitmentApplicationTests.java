a30ce16508fb5d5fd4c4aec304a9380a78690925
JRojowski <jacentylbn@gmail.com>
2024-11-24 11:41:27 +0100

    Disable contextLoads test

--- src/test/java/io/github/JRojowski/currency_recruitment/CurrencyRecruitmentApplicationTests.java
+++ src/test/java/io/github/JRojowski/currency_recruitment/CurrencyRecruitmentApplicationTests.java
@@ -1,9 +1,11 @@
 package io.github.JRojowski.currency_recruitment;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled(value = "Disabled as I force the default profile to be local, which requires connection to mySQL. This is just to ease checking.")
 class CurrencyRecruitmentApplicationTests {
 
 	@Test
